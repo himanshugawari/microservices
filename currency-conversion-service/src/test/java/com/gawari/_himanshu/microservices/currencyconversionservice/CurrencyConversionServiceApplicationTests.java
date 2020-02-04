284555a06235ee013e9534ee64e81ee37c470794
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-02-04 22:35:11 +0530

    initial commit

---
+++ currency-conversion-service/src/test/java/com/gawari/_himanshu/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gawari._himanshu.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
