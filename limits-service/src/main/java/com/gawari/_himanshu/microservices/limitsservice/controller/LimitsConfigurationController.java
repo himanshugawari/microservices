972526525293bb26a82aa464cf28ca6671276985
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-02-05 22:46:15 +0530

    added hystrix fault tolerance.

--- limits-service/src/main/java/com/gawari/_himanshu/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/gawari/_himanshu/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.gawari._himanshu.microservices.limitsservice.bean.LimitConfiguration;
 import com.gawari._himanshu.microservices.limitsservice.configuration.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -16,6 +17,16 @@ public class LimitsConfigurationController {
 	@GetMapping(path = "/limits")
 	public LimitConfiguration retrieveLimitsFromCongiguration() {
 		return new LimitConfiguration(conf.getMaximum(), conf.getMinimum());
+	}
+
+	@GetMapping(path = "/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveCongiguration")
+	public LimitConfiguration retrieveCongiguration() {
+		throw new RuntimeException("NOT AVAILABLE");
+	}
+
+	public LimitConfiguration fallbackRetrieveCongiguration() {
+		return new LimitConfiguration(999, 9);
 	}
 
 }
