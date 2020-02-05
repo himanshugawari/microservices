15c22d0724b1611d27b07510d19c80f9cc3653df
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-02-05 11:06:44 +0530

    added Ribbon client side load balancing

--- currency-conversion-service/src/main/java/com/gawari/_himanshu/microservices/currencyconversionservice/feignproxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/gawari/_himanshu/microservices/currencyconversionservice/feignproxy/CurrencyExchangeServiceProxy.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.gawari._himanshu.microservices.currencyconversionservice.bean.CurrencyConversion;
 
 //after using ribbon we don't need to put url in feign client
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
-//@FeignClient(name = "currency-exchange-service")
-//@RibbonClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
